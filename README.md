3a6fa944e8d86197019406e59fbbe0f7cbe56cde
Saqlain Mansab <saqlainmansab786@gmail.com>
2023-12-18 20:35:00 +0500

    Updated Readme file

--- README.md
+++ README.md
@@ -10,7 +10,36 @@ Pharma Management System
 Mr. Nazeef Ul Haq
 
 ### Group Members
-- Muhammad Tabish Akhtar
-- Saqlain Mansab
-- Sami Ullah
-- Muhammad Saad
+- Muhammad Tabish Akhtar (2022-CS-78)
+- Saqlain Mansab (2022-CS-80)
+- Sami Ullah (2022-CS-82)
+- Malik Muhammad Saad (2022-CS-94)
+
+### Instructions
+- Clone the repo using this link \
+  ` https://gitlab.com/dsa-lab/finalproject.git `
+- Move in the folder named as "Main Code" using this command.\
+  ` cd "Main Code" `
+- Run the program.py file.
+- The main screen of program will be available.
+
+### Packages
+- Python 3.11.3
+- pip 23.3.1
+- PyQt5
+- Pandas
+- Numpy
+- Matplotlib
+- Networkx
+
+### Terms and Conditions 
+
+### Paths
+No system depended paths are used. All paths are relative.
+
+```python
+def good_bye():
+    print("Thank you, for visiting the Repo...")
+
+good_bye()
+```
--- Saad and Sami/linearSearch.py
+++ Saad and Sami/linearSearch.py
@@ -1,17 +1,25 @@
 import pandas as pd
+
 def linearSearch(data, searchElement,index):
+    '''
+        data = 2d array
+        searchElement = word or query being searched
+        index = the index of column in which search is performed
+    '''
     see=[]
-    for i in range(0,len(data)):
+    for i in range(0, len(data)):
         # -------we use .lower function that convert string into small caps by doing this 
-        # -------we over come the  case senscetive 
+        # -------we over come the  case sencetive 
         if searchElement.lower() in data[i][index].lower():
             see.append(data[i])
 
     #see = pd.DataFrame(see,columns=["Product Name","Price","Condition","Shipping Price","Shipping City","Seller name","Feedback","Rating"])
     return see
 
-data = pd.read_csv("medicineData.csv")
-data = data.values.tolist()
-found=linearSearch(data,"sleep",0)
-for row in found:
-    print(row)
+
+if __name__ == "__main__":
+    data = pd.read_csv("medicineData.csv")
+    data = data.values.tolist()
+    found=linearSearch(data,"sleep",0)
+    for row in found:
+        print(row)
